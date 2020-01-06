f240a3d8137efcfc3b41b36b9892935171529945
airdry <flyside@bk.ru>
2020-01-06 14:02:47 +0300

    feat: add material theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,60 +1,67 @@
+const pkg = require("./package");
 
-export default {
-  mode: 'spa',
+module.exports = {
+  mode: "spa",
+
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons"
+      }
     ]
   },
+
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#9ccc65", height: "10px" },
+
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
+    { src: "vue-material/dist/vue-material.min.css", lang: "css" },
+    { src: "~/assets/theme.scss", lang: "scss" }
   ],
+
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: "~/plugins/vue-material" }],
+
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    "@nuxtjs/axios"
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   */
   axios: {
+    // See https://github.com/nuxt-community/axios-module#options
   },
+
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
